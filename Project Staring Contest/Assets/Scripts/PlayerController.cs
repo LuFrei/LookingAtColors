efa22d0360257b46cb49ba35b1a17246779d7bc1
Lucas Freitas <lucasfreitas09876@gmail.com>
2019-06-17 22:19:54 -0400

    Basic movements in Player Controller Done
    Still need to make camera Y axis rotate on World axis

--- Project Staring Contest/Assets/Scripts/PlayerController.cs
+++ Project Staring Contest/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	public Transform camera;
 
+	private void FixedUpdate()
+	{
+		Move(GetMoveInput());
+		Look(GetMouseInput());
+	}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	Vector3 GetMoveInput() {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		return new Vector3(horizontal, 0, vertical);
+	}
+
+	Vector2 GetMouseInput()
+	{
+		float horizontal = Input.GetAxis("Mouse X");
+		float vertical = Input.GetAxis("Mouse Y");
+
+		return new Vector2(vertical, horizontal);	//These are flipped because we are rotating the Y axis on x movement and vice versa
+	}
+
+	void Move(Vector3 moveVector) {
+		transform.Translate(moveVector);
+	}
+
+	void Look(Vector3 lookVector)
+	{
+		camera.Rota(lookVector);
+	}
 }
