05ed06eeb0d2a3a6e73e2e8ef179d0234878a45c
Lucas Freitas <lucasfreitas09876@gmail.com>
2019-06-19 07:56:38 -0400

    PC Sync
    Started limiting Cam x rotation

--- Project Staring Contest/Assets/Scripts/PlayerController.cs
+++ Project Staring Contest/Assets/Scripts/PlayerController.cs
@@ -5,34 +5,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	public Transform camera;
+	public float lookSensitivity;
+	public float moveSpeed;
 
-	private void FixedUpdate()
-	{
+	private void FixedUpdate(){
 		Move(GetMoveInput());
 		Look(GetMouseInput());
 	}
 
 	Vector3 GetMoveInput() {
-		float horizontal = Input.GetAxis("Horizontal");
-		float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+		float vertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
 		return new Vector3(horizontal, 0, vertical);
 	}
 
-	Vector2 GetMouseInput()
-	{
+	Vector2 GetMouseInput(){
 		float horizontal = Input.GetAxis("Mouse X");
 		float vertical = Input.GetAxis("Mouse Y");
 
-		return new Vector2(vertical, horizontal);	//These are flipped because we are rotating the Y axis on x movement and vice versa
+		return new Vector2(vertical, horizontal);		//These are flipped because we are rotating the Y axis on x movement and vice versa
 	}
 
 	void Move(Vector3 moveVector) {
 		transform.Translate(moveVector);
 	}
 
-	void Look(Vector3 lookVector)
-	{
-		camera.Rota(lookVector);
+	void Look(Vector3 lookVector){
+		camera.Rotate(Vector2.left * lookVector.x * lookSensitivity);		//rotate camera up and down
+		transform.Rotate(Vector2.up * lookVector.y * lookSensitivity);  //roate body left and right
+
+		//Camera x rot Limits
+		Mathf.Clamp(camera.rotation.x, -90, 90);
+		
 	}
 }
